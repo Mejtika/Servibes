cd5587edf807de10907a524e50935debe9610130
Mateusz Golombniak <Mejtika@hotmail.com>
2020-11-23 22:18:08 +0100

    Add events and message broker abstractions

---
+++ src/Shared/Servibes.Shared/Communication/Events/EventMapperCompositionRoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Servibes.Shared.Communication
+{
+    internal sealed class EventMapperCompositionRoot : IEventMapperCompositionRoot
+    {
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+
+        public EventMapperCompositionRoot(IServiceScopeFactory serviceScopeFactory)
+            => _serviceScopeFactory = serviceScopeFactory;
+
+        public INotification Map(IDomainEvent domainEvent)
+        {
+            using var scope = _serviceScopeFactory.CreateScope();
+            var mappers = scope.ServiceProvider.GetService<IEnumerable<IEventMapper>>();
+
+            return mappers
+                ?.Select(mapper => mapper.Map(domainEvent))
+                .SingleOrDefault(@event => @event is { });
+        }
+
+        public IEnumerable<INotification> MapAll(IEnumerable<IDomainEvent> domainEvents)
+            => domainEvents.Select(Map);
+    }
+}
---
+++ src/Shared/Servibes.Shared/Communication/Events/IEventMapperCompositionRoot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace Servibes.Shared.Communication
+{
+    public interface IEventMapperCompositionRoot
+    {
+        INotification Map(IDomainEvent domainEvent);
+        IEnumerable<INotification> MapAll(IEnumerable<IDomainEvent> domainEvents);
+    }
+}
