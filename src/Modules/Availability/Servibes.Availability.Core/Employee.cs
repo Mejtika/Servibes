dda6d37cb7902e18fd00c4d3b09bfa638c7735e0
Mateusz Golombniak <Mejtika@hotmail.com>
2020-12-04 13:24:32 +0100

    Add domain classes to core

---
+++ src/Modules/Availability/Servibes.Availability.Core/Employee.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Servibes.Shared.BuildingBlocks;
+
+namespace Servibes.Availability.Core
+{
+    public class Employee : AggregateRoot
+    {
+        public Guid EmployeeId { get; set; }
+
+        public WorkingHours WorkingHours { get; set; }
+
+        private ISet<Reservation> _reservations = new HashSet<Reservation>();
+
+        public IEnumerable<Reservation> Reservations
+        {
+            get => _reservations;
+            private set => _reservations = new HashSet<Reservation>(value);
+        }
+
+        public Employee(Guid employeeId, List<WorkingHours> workingHours)
+        {
+            EmployeeId = employeeId;
+            Reservations = Enumerable.Empty<Reservation>();
+        }
+
+        public static Employee Create(Guid employeeId, List<WorkingHours> workingHours)
+        {
+            var resource = new Employee(employeeId, workingHours);
+
+            // resource.AddEvent(new ResourceCreated(resource));
+
+            return resource;
+        }
+
+        public void AddReservation(Reservation reservation)
+        { 
+            var isInWeekWorkingRange = IsInWeekWorkingRange(reservation);
+            var hasCollidingReservation = _reservations.Any(IsCollidingReservation);
+            if (hasCollidingReservation && isInWeekWorkingRange)
+            {
+                AddEvent(new ReservationCanceled(this, reservation));
+                return;
+            }
+
+            if (_reservations.Add(reservation))
+            {
+                AddEvent(new ReservationAdded(this, reservation));
+            }
+
+            bool IsCollidingReservation(Reservation r) => r.IsCollidingWith(reservation);
+
+        }
+
+        private bool IsInWeekWorkingRange(Reservation reservation)
+            => !reservation.IsLongPeriodReservation() && !WorkingHours.IsOutOfRange(reservation);
+        
+        public void ChangeAvailability(WorkingHours workingHours) => WorkingHours = workingHours;
+
+        public void ReleaseReservation(Reservation reservation)
+        {
+            if (!_reservations.Remove(reservation))
+            {
+                return;
+            }
+
+            AddEvent(new ReservationReleased(this, reservation));
+        }
+
+        public void Delete()
+        {
+            foreach (var reservation in Reservations)
+            {
+                AddEvent(new ReservationCanceled(this, reservation));
+            }
+
+            AddEvent(new ResourceDeleted(this));
+        }
+    }
+}
---
+++ src/Modules/Availability/Servibes.Availability.Core/WorkingHours.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Servibes.Availability.Core
+{
+    public class HoursRange
+    {
+        public HoursRange(bool isAvailable, TimeSpan start, TimeSpan end)
+        {
+            IsAvailable = isAvailable;
+            Start = start;
+            End = end;
+        }
+
+        public bool IsAvailable { get; }
+        public TimeSpan Start { get; }
+        public TimeSpan End { get; }
+
+        public bool IsCollidingWith(TimeSpan start, TimeSpan end)
+            => !IsAvailable || !(end <= Start || start >= End);
+    }
+
+    public class WorkingHours
+    {
+        public WorkingHours(
+            HoursRange monday,
+            HoursRange tuesday,
+            HoursRange wednesday,
+            HoursRange thursday,
+            HoursRange friday,
+            HoursRange saturday,
+            HoursRange sunday)
+        {
+            Monday = monday;
+            Tuesday = tuesday;
+            Wednesday = wednesday;
+            Thursday = thursday;
+            Friday = friday;
+            Saturday = saturday;
+            Sunday = sunday;
+        }
+
+        private HoursRange Monday { get; }
+        private HoursRange Tuesday { get; }
+        private HoursRange Wednesday { get; }
+        private HoursRange Thursday { get; }
+        private HoursRange Friday { get; }
+        private HoursRange Saturday { get; }
+        private HoursRange Sunday { get; }
+
+        public bool IsOutOfRange(Reservation reservation)
+            => reservation.Start.DayOfWeek switch
+            {
+                DayOfWeek.Monday => Monday.IsCollidingWith(reservation.Start.TimeOfDay, reservation.End.TimeOfDay),
+                DayOfWeek.Tuesday => Tuesday.IsCollidingWith(reservation.Start.TimeOfDay, reservation.End.TimeOfDay),
+                DayOfWeek.Wednesday => Wednesday.IsCollidingWith(reservation.Start.TimeOfDay, reservation.End.TimeOfDay),
+                DayOfWeek.Thursday => Thursday.IsCollidingWith(reservation.Start.TimeOfDay, reservation.End.TimeOfDay),
+                DayOfWeek.Friday => Friday.IsCollidingWith(reservation.Start.TimeOfDay, reservation.End.TimeOfDay),
+                DayOfWeek.Saturday => Saturday.IsCollidingWith(reservation.Start.TimeOfDay, reservation.End.TimeOfDay),
+                DayOfWeek.Sunday => Sunday.IsCollidingWith(reservation.Start.TimeOfDay, reservation.End.TimeOfDay),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+    }
+
+
+    //private static void CheckForDaysCorrectness(IEnumerable<WorkingHours> workingHours)
+    //{
+    //    if (workingHours.ToList().Count != Enum.GetNames(typeof(DayOfWeek)).Length)
+    //    {
+    //        throw new Exception();
+    //    }
+
+    //    var daysOfTheWeek = Enum.GetNames(typeof(DayOfWeek)).ToList();
+    //    var daysInWorkignHours = workingHours.Select(x => x.DayOfWeek.ToString()).ToList();
+    //    if (daysOfTheWeek.Except(daysInWorkignHours).Any())
+    //    {
+    //        throw new Exception();
+    //    }
+    //}
+}
