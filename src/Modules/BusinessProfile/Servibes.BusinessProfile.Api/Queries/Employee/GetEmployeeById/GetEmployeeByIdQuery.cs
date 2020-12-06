f909c75abb95bc159242a5f38eee61e7ec10ee1d
Arkadiusz Kowalczyk <dev.triplea@gmail.com>
2020-12-06 17:53:02 +0100

    Added Employee queries.

---
+++ src/Modules/BusinessProfile/Servibes.BusinessProfile.Api/Queries/Employee/GetEmployeeById/GetEmployeeByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servibes.BusinessProfile.Api.Queries.Employees.GetEmployeeById
+{
+    public class GetEmployeeByIdQuery : IRequest<CompanyEmployeesDto>
+    {
+        public Guid CompanyId { get; set; }
+        public Guid EmployeeId { get; set; }
+    }
+}
---
+++ src/Modules/BusinessProfile/Servibes.BusinessProfile.Api/Queries/Employee/GetEmployeeById/GetEmployeeByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Servibes.BusinessProfile.Api.Queries.Employees.GetEmployeeById
+{
+    public class GetEmployeeByIdQueryHandler : IRequestHandler<GetEmployeeByIdQuery, CompanyEmployeesDto>
+    {
+        private readonly BusinessProfileContext context;
+        private readonly IMapper mapper;
+
+        public GetEmployeeByIdQueryHandler(BusinessProfileContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public Task<CompanyEmployeesDto> Handle(GetEmployeeByIdQuery request, CancellationToken cancellationToken)
+        {
+            var employee = context.Employees.Where(e => e.EmployeeId == request.EmployeeId && e.CompanyId == request.CompanyId);
+
+            if (employee == null)
+                throw new ArgumentException($"Employee with id {request.EmployeeId} and company id {request.CompanyId} doesnt exist.");
+
+            return Task.FromResult(mapper.Map<CompanyEmployeesDto>(employee));
+        }
+    }
+}
