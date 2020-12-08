e7c03df4c1770c74868ffe35f606afc54c7b74f9
Arkadiusz Kowalczyk <dev.triplea@gmail.com>
2020-12-08 23:54:45 +0100

    Updated company page to use newly created data services

--- src/Servibes.Bootstrapper/ClientApp/src/app/client-main/company-page/company-page.component.ts
+++ src/Servibes.Bootstrapper/ClientApp/src/app/client-main/company-page/company-page.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CompanyDataService } from 'src/app/data-service/company-data.servce';
+import { EmployeeDataService } from 'src/app/data-service/employee-data.service';
 import { MockDataService } from 'src/app/data-service/mock-data.service';
+import { ServicesDataService } from 'src/app/data-service/services-data.service';
 
 import { ICompany } from 'src/app/shared/interfaces/company';
 
@@ -13,16 +16,37 @@ export class CompanyPageComponent {
     company: ICompany;
 
     constructor(
-        private dataService: MockDataService,
+        private companyDataService: CompanyDataService,
+        private servicesDataService: ServicesDataService,
+        private employeeDataService: EmployeeDataService,
         private activatedRoute: ActivatedRoute) {
 
     }
 
     ngOnInit() {
-        const id: string = this.activatedRoute.snapshot.paramMap.get('id');
+        const companyId: string = this.activatedRoute.snapshot.paramMap.get('id');
 
-        this.dataService.getCompany(id).subscribe(result => {
+        /*this.dataService.getCompany(id).subscribe(result => {
             this.company = result;
+        });*/
+
+        //TODO: Fix 3 api calls into 1 using rxjs
+
+        this.companyDataService.getCompanyById(companyId).subscribe(result => {
+          this.company = result;
+          console.log('company', result);
+
+          this.servicesDataService.getAllCompanyServices(companyId).subscribe(result => {
+            this.company.services = result;
+            console.log('services', result);
+          });
+
+          this.employeeDataService.getAllCompanyEmployees(companyId).subscribe(result => {
+            this.company.employees = result;
+            console.log('employees', result);
+          });
         });
+
+
     }
 }
