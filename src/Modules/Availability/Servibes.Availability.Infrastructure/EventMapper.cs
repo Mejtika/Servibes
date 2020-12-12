2967311d932876ea221bc1dedee3589f83eb10f3
Mateusz Golombniak <Mejtika@hotmail.com>
2020-12-12 01:28:43 +0100

    Added external AppointmentCreated event and update event mapper

---
+++ src/Modules/Availability/Servibes.Availability.Infrastructure/EventMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MediatR;
+using Servibes.Availability.Application.Events.ReservationAdded;
+using Servibes.Availability.Core.Employees.Events;
+using Servibes.Shared.BuildingBlocks;
+using Servibes.Shared.Communication.Events;
+
+namespace Servibes.Availability.Infrastructure
+{
+    public class EventMapper : IEventMapper
+    {
+        public INotification Map(IDomainEvent domainEvent)
+            => domainEvent switch
+            {
+                EmployeeReservationAddedDomainEvent @event => new ReservationAddedEvent(@event.Employee.EmployeeId, @event.Reservation.Start),
+                _ => null
+            };
+
+        public IEnumerable<INotification> MapAll(IEnumerable<IDomainEvent> domainEvents)
+            => domainEvents.Select(Map);
+    }
+}
