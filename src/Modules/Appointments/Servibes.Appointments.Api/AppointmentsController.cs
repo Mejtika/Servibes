1d4f76d141dad024cc948ec706e44d375c4079a2
Mateusz Golombniak <Mejtika@hotmail.com>
2020-12-12 01:29:16 +0100

    Added appointment controller and make appointment endpoint

---
+++ src/Modules/Appointments/Servibes.Appointments.Api/AppointmentsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Servibes.Appointments.Application.Appointments.MakeAppointment;
+
+namespace Servibes.Appointments.Api
+{
+    [ApiController]
+    [Route("api/companies")]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AppointmentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("{companyId}/employees/{employeeId}/appointments")]
+        public async Task<IActionResult> MakeAppointment(Guid companyId, Guid employeeId, [FromBody] MakeAppointmentRequest request)
+        {
+            await _mediator.Send(new MakeAppointmentCommand(
+                companyId, 
+                employeeId,
+                request.EmployeeName,
+                request.ServiceName,
+                request.ServicePrive,
+                request.ServiceDuration,
+                request.Start));
+
+            return NoContent();
+        }
+    }
+}
--- src/Modules/Appointments/Servibes.Appointments.Api/AppointmentsModuleExtensions.cs
+++ src/Modules/Appointments/Servibes.Appointments.Api/AppointmentsModuleExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Servibes.Appointments.Application;
 using Servibes.Appointments.Infrastructure;
 
 namespace Servibes.Appointments.Api
@@ -10,12 +11,14 @@ namespace Servibes.Appointments.Api
         public static IServiceCollection AddAppointmentsModule(this IServiceCollection services, IConfiguration configuration)
         {
             return services
+                .AddApplication()
                 .AddInfrastructure(configuration);
         }
 
         public static IApplicationBuilder UseAppointmentsModule(this IApplicationBuilder app)
         {
             return app
+                .UseApplication()
                 .UseInfrastructure();
         }
     }
