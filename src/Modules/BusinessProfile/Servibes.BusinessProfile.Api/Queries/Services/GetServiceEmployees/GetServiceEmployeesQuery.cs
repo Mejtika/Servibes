bba61714c2558383e2fc714d5c2bdcf455864ad9
Arkadiusz Kowalczyk <dev.triplea@gmail.com>
2020-12-13 09:19:34 +0100

    Added endpoint for collecting all performers for service.

---
+++ src/Modules/BusinessProfile/Servibes.BusinessProfile.Api/Queries/Services/GetServiceEmployees/GetServiceEmployeesQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Servibes.BusinessProfile.Api.Queries.Employees;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servibes.BusinessProfile.Api.Queries.Services.GetServiceEmployees
+{
+    public class GetServiceEmployeesQuery : IRequest<IEnumerable<CompanyEmployeeDto>>
+    {
+        public Guid ServiceId { get; set; }
+    }
+}
