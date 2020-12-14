32da260b963a948814480fb6d581e8f464f7d43f
Arkadiusz Kowalczyk <dev.triplea@gmail.com>
2020-12-14 17:24:11 +0100

    Company page component clean up

--- src/Servibes.Bootstrapper/ClientApp/src/app/client-main/company-page/company-page.component.ts
+++ src/Servibes.Bootstrapper/ClientApp/src/app/client-main/company-page/company-page.component.ts
@@ -1,14 +1,11 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { formatDate } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 
 import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
 
 import { CompanyDataService } from 'src/app/data-service/company-data.servce';
 import { EmployeeDataService } from 'src/app/data-service/employee-data.service';
-import { MockDataService } from 'src/app/data-service/mock-data.service';
 import { ServicesDataService } from 'src/app/data-service/services-data.service';
-import { AppointmentDataService } from 'src/app/data-service/appointment-data.service';
 
 import { ICompany, IService, IEmployee, IServiceHours } from 'src/app/shared/interfaces/company';
 import { forkJoin } from 'rxjs';
@@ -42,7 +39,6 @@ export class CompanyPageComponent {
         private companyDataService: CompanyDataService,
         private servicesDataService: ServicesDataService,
         private employeeDataService: EmployeeDataService,
-        private appointmentDataService: AppointmentDataService,
         private activatedRoute: ActivatedRoute,
         private modalService: BsModalService) {
 
@@ -52,8 +48,6 @@ export class CompanyPageComponent {
         const companyId: string = this.activatedRoute.snapshot.paramMap.get('id');
 
         this.loadCompanyData(companyId);
-
-        //this.selectedEmployee = null;
     }
 
     loadCompanyData(companyId: string) {
@@ -86,58 +80,5 @@ export class CompanyPageComponent {
       this.modalRef.onHide.asObservable().subscribe(x => {
         this.modalService.show(ConfirmModalComponent);
       });
-
-       //this.servicesDataService.getServiceEmployees(this.company.companyId, service.serviceId).subscribe(result => {
-       //  console.log('companyId', this.company.companyId);
-       //  console.log('serviceId', service.serviceId);
-
-       //  console.log('serviceEmployees', result);
-
-       //  this.serviceEmployees = result;
-       //});
     }
-
-     //public selectEmployee(employee: IEmployee) {
-     //  this.selectedEmployee = employee;
-
-     //  console.log('selectedDate', this.selectedDate);
-
-     //  this.employeeDataService.getEmployeeDayAvailability(this.company.companyId, this.selectedEmployee.employeeId, formatDate(this.selectedDate, 'yyyy-MM-dd', 'en_US'), this.selectedService.duration).subscribe(result => {
-     //    this.serviceAvailableHours = result;
-
-     //    console.log('serviceAvailableHours', result);
-     //  });
-     //}
-
-     //public createAppointment(hour: IServiceHours) {
-     //    console.log('created appointment on ', hour);
-
-     //  let appointment = this.createAppointmentObject(this.selectedEmployee, this.selectedService, this.selectedDate, hour);
-
-     //  this.appointmentDataService.postAppointment(this.company.companyId, this.selectedEmployee.employeeId, appointment).subscribe(result => {
-     //    console.log('Appointment created: ', appointment);
-     //  });
-
-     //  this.resetSelectedData();
-     //  this.modal.closeModalOnEscape();
-     //}
-
-     //public resetSelectedData() {
-     //    this.selectedDate = new Date();
-     //    this.serviceEmployees = null;
-     //    this.selectedEmployee = null;
-     //    this.selectedService = null;
-     //    this.serviceAvailableHours = null;
-     //}
-
-     //private createAppointmentObject(employee: IEmployee, service: IService, selectedDate: Date, selectedHour: IServiceHours) {
-     //    return {
-     //      employeeName: employee.firstName + " " + employee.lastName,
-     //      serviceName: service.serviceName,
-     //      servicePrice: service.price,
-     //      serviceDuration: service.duration,
-     //      start: formatDate(selectedDate, 'yyy-MM-dd', "en_US") + "T" + selectedHour.time
-     //    };
-     //}
-
 }
