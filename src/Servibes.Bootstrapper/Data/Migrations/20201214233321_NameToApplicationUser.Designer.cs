ef58b3b4126ad5d64973f9caef79ac57ec05ab47
Mateusz Golombniak <Mejtika@hotmail.com>
2020-12-15 13:06:34 +0100

    Added name and surname to user; Added message broker and event

--- src/Servibes.Bootstrapper/Data/Migrations/20201214233321_NameToApplicationUser.Designer.cs
+++ src/Servibes.Bootstrapper/Data/Migrations/20201214233321_NameToApplicationUser.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Servibes.Bootstrapper.Data;
 
 namespace Servibes.Bootstrapper.Data.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20201214233321_NameToApplicationUser")]
+    partial class NameToApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
