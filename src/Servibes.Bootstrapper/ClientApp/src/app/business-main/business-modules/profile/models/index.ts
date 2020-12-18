b49cd317a744357a10b2d4a907e24358c6f9dbb6
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@onwelo.com>
2020-12-18 01:08:30 +0100

    Added business profile models

--- src/Servibes.Bootstrapper/ClientApp/src/app/business-main/business-modules/profile/models/index.ts
+++ src/Servibes.Bootstrapper/ClientApp/src/app/business-main/business-modules/profile/models/index.ts
@@ -1,1 +1,4 @@
 export * from './profile.model';
+export * from './employee.model';
+export * from './service.model';
+export * from './category.model';
--- src/Servibes.Bootstrapper/ClientApp/src/app/business-main/business-modules/profile/models/profile.model.ts
+++ src/Servibes.Bootstrapper/ClientApp/src/app/business-main/business-modules/profile/models/profile.model.ts
@@ -1,1 +1,17 @@
-export {};
+export interface IProfile {
+    companyId: string;
+    companyName: string;
+    phoneNumber: string;
+    category: string;
+    description: string;
+    coverPhoto: string;
+    address: IAddress;
+}
+
+export interface IAddress {
+    city: string;
+    zipcode: string;
+    street: string;
+    flatNumber: string;
+    streetNumber: string;
+}
