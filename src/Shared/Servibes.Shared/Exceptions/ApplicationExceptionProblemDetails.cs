01ff224d591dfe57cf8f6b87cec79b2401b0e753
Mateusz Golombniak <Mejtika@hotmail.com>
2020-12-18 13:39:37 +0100

    Added domain/app exceptions mappings

---
+++ src/Shared/Servibes.Shared/Exceptions/ApplicationExceptionProblemDetails.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Servibes.Shared.Exceptions
+{
+    public class ApplicationExceptionProblemDetails : ProblemDetails
+    {
+        public List<string> Errors { get; }
+
+        public ApplicationExceptionProblemDetails(AppException exception)
+        {
+            this.Title = "Validation error";
+            this.Status = StatusCodes.Status400BadRequest;
+            this.Detail = "One or more errors occurred";
+            this.Errors = exception.Errors;
+            this.Type = "https://servibes/validation-error";
+        }
+    }
+}
