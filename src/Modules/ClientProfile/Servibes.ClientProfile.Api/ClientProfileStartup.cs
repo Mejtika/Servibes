71a1af8052302a81652920861caddc69abc1df23
Mateusz Golombniak <Mejtika@hotmail.com>
2020-12-18 17:34:39 +0100

    Added client profile module

---
+++ src/Modules/ClientProfile/Servibes.ClientProfile.Api/ClientProfileStartup.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Servibes.Shared;
+
+namespace Servibes.ClientProfile.Api
+{
+    public static class ClientProfileStartup
+    {
+        public static IServiceCollection AddClientProfileModule(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddDbContext<ClientProfileContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    sqlServerOptions =>
+                    {
+                        sqlServerOptions.MigrationsHistoryTable("__ClientProfileMigrationsHistory", "client");
+                    });
+            });
+
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseClientProfileModule(this IApplicationBuilder app)
+        {
+            app.UseModuleRequests();
+
+            return app;
+        }
+    }
+}
