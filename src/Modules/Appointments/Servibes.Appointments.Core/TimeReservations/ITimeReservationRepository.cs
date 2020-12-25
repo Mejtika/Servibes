ba199fffb88e7871321285e20f7e2e9fd5d94b89
Mateusz Golombniak <Mejtika@hotmail.com>
2020-12-25 17:14:54 +0100

    Added another appointment migration and authorization client

--- src/Modules/Appointments/Servibes.Appointments.Core/TimeReservations/ITimeReservationRepository.cs
+++ src/Modules/Appointments/Servibes.Appointments.Core/TimeReservations/ITimeReservationRepository.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Servibes.Appointments.Core.TimeReservations
 {
     public interface ITimeReservationRepository
     {
         Task AddAsync(TimeReservation timeReservation);
+
+        Task<TimeReservation> GetAsync(Guid timeReservationId);
     }
 }
