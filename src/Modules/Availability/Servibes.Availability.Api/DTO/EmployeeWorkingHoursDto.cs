dc70f56d7fb674933ba0051b2b1759cbc8111295
Mateusz Golombniak <Mejtika@hotmail.com>
2020-12-25 17:18:28 +0100

    Renamed availability dtos to requests objects

--- src/Modules/Availability/Servibes.Availability.Api/DTO/EmployeeWorkingHoursDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Servibes.Availability.Api.DTO
-{
-    public class EmployeeWorkingHoursDto
-    {
-        public List<HoursRangeDto> WorkingHours { get; set; }
-    }
-}
