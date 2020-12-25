ac09175940da030e07d80b9ff0f34cc88831236d
Mateusz Golombniak <Mejtika@hotmail.com>
2020-12-25 17:14:06 +0100

    Added appointments endpoints

---
+++ src/Modules/Appointments/Servibes.Appointments.Application/Appointments/CancelBusinessAppointment/CancelBusinessAppointmentCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MediatR;
+
+namespace Servibes.Appointments.Application.Appointments.CancelBusinessAppointment
+{
+    public class CancelBusinessAppointmentCommand : IRequest
+    {
+        public Guid AppointmentId { get; }
+
+        public string CancellationReason { get; }
+
+        public CancelBusinessAppointmentCommand(Guid appointmentId, string cancellationReason)
+        {
+            AppointmentId = appointmentId;
+            CancellationReason = cancellationReason;
+        }
+    }
+}
---
+++ src/Modules/Appointments/Servibes.Appointments.Application/Appointments/CancelBusinessTimeReservation/CancelBusinessTimeReservationCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MediatR;
+
+namespace Servibes.Appointments.Application.Appointments.CancelBusinessTimeReservation
+{
+    public class CancelBusinessTimeReservationCommand : IRequest
+    {
+        public Guid TimeReservationId { get; }
+
+        public CancelBusinessTimeReservationCommand(Guid timeReservationId)
+        {
+            TimeReservationId = timeReservationId;
+        }
+    }
+}
