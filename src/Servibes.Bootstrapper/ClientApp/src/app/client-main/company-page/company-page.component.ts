3ce42417c12a988f7297fa6b03bbd51598f7d64a
Mateusz Golombniak <Mejtika@hotmail.com>
2020-12-28 22:24:53 +0100

    Some simple refactorings of company-page

--- src/Servibes.Bootstrapper/ClientApp/src/app/client-main/company-page/company-page.component.ts
+++ src/Servibes.Bootstrapper/ClientApp/src/app/client-main/company-page/company-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from "@angular/core";
+import { Component } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { BsModalService } from "ngx-bootstrap/modal";
 import { CompanyDataService } from "src/app/data-service/company-data.service";
@@ -14,8 +14,11 @@ import { forkJoin } from "rxjs";
 import { ClientReservationComponent } from "./client-reservation/client-reservation.component";
 import { ClientFavoritesService } from "../client-account/client-favorites.service";
 import { ToastrService } from "ngx-toastr";
-import { ClientReviewsService, ReviewSummaryDto } from '../client-account/client-reviews.service';
-import { ReviewDetails } from '../client-account/client-reviews/client-reviews.component';
+import {
+  ClientReviewsService,
+  ReviewSummaryDto,
+} from "../client-account/client-reviews.service";
+import { ReviewDetails } from "../client-account/client-reviews/client-reviews.component";
 
 @Component({
   selector: "company-page",
@@ -34,16 +37,11 @@ export class CompanyPageComponent {
     "Saturday",
   ];
   public selectedService: IService;
-
   public selectedDate: Date = new Date();
-
   public serviceEmployees: IEmployee[];
   public selectedEmployee: IEmployee;
-
   public serviceAvailableHours: IServiceHours[];
-
   public isFavoriteCompany: boolean;
-
   public reviewsSummary: ReviewSummaryDto;
   public reviews: ReviewDetails[];
 
@@ -67,16 +65,26 @@ export class CompanyPageComponent {
     const companyId: string = this.activatedRoute.snapshot.paramMap.get("id");
     this.loadCompanyData(companyId);
 
-    this.favoritesService.favoritesCompanies$.subscribe((companies) => {
+    this.favoritesService
+    .favoritesCompanies$
+    .subscribe(companies => 
       this.isFavoriteCompany = companies
-      .find((c) => c.companyId == this.company.companyId) !== undefined;
-    });
+      .find(company => company.companyId == this.company.companyId) !== undefined
+    );
+    
+    this.reviewsService
+    .getCompanyReviews(companyId)
+    .subscribe(result => this.reviews = result);
+
+    this.reviewsService
+      .getReviewsSummary(companyId)
+      .subscribe(result => this.reviewsSummary = result);
   }
 
   addToFavorites() {
     this.favoritesService
       .addToFavorites(this.company.companyId)
-      .subscribe((_) => {
+      .subscribe(_ => {
         this.toastr.success("Company was successfully added to favorites!");
         this.refreshPage();
       });
@@ -85,50 +93,37 @@ export class CompanyPageComponent {
   deleteFromFavorites() {
     this.favoritesService
       .deleteFromFavorites(this.company.companyId)
-      .subscribe((_) => {
+      .subscribe(_ => {
         this.toastr.success("Company was successfully removed from favorites!");
         this.refreshPage();
       });
   }
 
   loadCompanyData(companyId: string) {
-    let company = this.companyDataService.getCompanyById(companyId);
-    let openingHours = this.companyDataService.getCompanyOpeningHours(
-      companyId
-    );
-    let services = this.servicesDataService.getAllCompanyServices(companyId);
-    let employees = this.employeeDataService.getAllCompanyEmployees(companyId);
+    let company$ = this.companyDataService.getCompanyById(companyId);
+    let openingHours$ = this.companyDataService.getCompanyOpeningHours(companyId);
+    let services$ = this.servicesDataService.getAllCompanyServices(companyId);
+    let employees$ = this.employeeDataService.getAllCompanyEmployees(companyId);
 
-    forkJoin([company, openingHours, services, employees]).subscribe(
-      (result) => {
-        result[0].openingHours = result[1];
-        result[0].services = result[2];
-        result[0].employees = result[3];
-
-        this.company = result[0];
-
-        this.reviewsService.getReviewsSummary(this.company.companyId)
-        .subscribe(result => this.reviewsSummary = result);
-
-        this.reviewsService.getCompanyReview(this.company.companyId)
-        .subscribe(result => this.reviews = result)
+    forkJoin([company$, openingHours$, services$, employees$]).subscribe(
+      ([company, openingHours, services, employees]) => {
+        company.openingHours = openingHours;
+        company.services = services;
+        company.employees = employees;
+        this.company = company;
       }
     );
-    
   }
 
   public selectService(service: IService) {
     this.selectedService = service;
-
     const initialState = {
       service: service,
       company: this.company,
     };
-
-    console.log("initialState", initialState);
-
     this.modalService.show(ClientReservationComponent, { initialState });
   }
+
   refreshPage() {
     this.router.navigateByUrl(`/client/companies/${this.company.companyId}`);
   }
