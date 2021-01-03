a6669458d74d81edfff1dbd1ea9e8cd7cf23ba89
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@onwelo.com>
2021-01-03 19:53:46 +0100

    Added migrations for image

---
+++ src/Modules/BusinessProfile/Servibes.BusinessProfile.Api/Migrations/20210103180049_ImageFileType.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Servibes.BusinessProfile.Api.Migrations
+{
+    public partial class ImageFileType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileType",
+                schema: "business",
+                table: "Images",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileType",
+                schema: "business",
+                table: "Images");
+        }
+    }
+}
