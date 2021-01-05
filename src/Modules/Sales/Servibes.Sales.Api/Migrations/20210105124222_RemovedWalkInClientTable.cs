b3366e155d1084ff1a934251e20b8901af0a3722
Mateusz Golombniak <Mejtika@hotmail.com>
2021-01-05 19:15:17 +0100

    Removed walkinclient entity from sales module

---
+++ src/Modules/Sales/Servibes.Sales.Api/Migrations/20210105124222_RemovedWalkInClientTable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Servibes.Sales.Api.Migrations
+{
+    public partial class RemovedWalkInClientTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WalkInClients",
+                schema: "sales");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_ReserveeId",
+                schema: "sales",
+                table: "Appointments",
+                column: "ReserveeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Clients_ReserveeId",
+                schema: "sales",
+                table: "Appointments",
+                column: "ReserveeId",
+                principalSchema: "sales",
+                principalTable: "Clients",
+                principalColumn: "ClientId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Clients_ReserveeId",
+                schema: "sales",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_ReserveeId",
+                schema: "sales",
+                table: "Appointments");
+
+            migrationBuilder.CreateTable(
+                name: "WalkInClients",
+                schema: "sales",
+                columns: table => new
+                {
+                    WalkInClientId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WalkInClients", x => x.WalkInClientId);
+                });
+        }
+    }
+}
