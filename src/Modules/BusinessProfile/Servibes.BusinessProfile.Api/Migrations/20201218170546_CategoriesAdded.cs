5a96467833505caec5d2518208ff4e02653a4243
Mateusz Golombniak <Mejtika@hotmail.com>
2021-01-09 16:20:59 +0100

    Changed business catergory name

--- src/Modules/BusinessProfile/Servibes.BusinessProfile.Api/Migrations/20201218170546_CategoriesAdded.cs
+++ src/Modules/BusinessProfile/Servibes.BusinessProfile.Api/Migrations/20201218170546_CategoriesAdded.cs
@@ -13,7 +13,7 @@ namespace Servibes.BusinessProfile.Api.Migrations
                 columns: new[] { "CategoryId", "Name" },
                 values: new object[,]
                 {
-                    { new Guid("d8f762ca-b7a5-486d-a793-e142f856d103"), "Hairdresser" },
+                    { new Guid("d8f762ca-b7a5-486d-a793-e142f856d103"), "Hair Salon" },
                     { new Guid("40b22590-3f08-4928-a86c-5c1d5d75d3bb"), "Barber" },
                     { new Guid("87467e7d-4185-4bd1-bc32-1a0f62f5726a"), "Massage" },
                     { new Guid("3ac00a98-2b01-46d5-99f2-546b9e302b18"), "Makeup" }
