d2754803c37c17dbf5430a3c8f82755bf0872d37
Mateusz Golombniak <Mejtika@hotmail.com>
2021-01-29 11:00:16 +0100

    Add relation between Image and Company in BusinessProfile module

---
+++ src/Modules/BusinessProfile/Servibes.BusinessProfile.Api/Migrations/20210119214243_AddRelationshipToImageCompany.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Servibes.BusinessProfile.Api.Migrations
+{
+    public partial class AddRelationshipToImageCompany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "business",
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: new Guid("3ac00a98-2b01-46d5-99f2-546b9e302b18"));
+
+            migrationBuilder.DeleteData(
+                schema: "business",
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: new Guid("40b22590-3f08-4928-a86c-5c1d5d75d3bb"));
+
+            migrationBuilder.DeleteData(
+                schema: "business",
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: new Guid("87467e7d-4185-4bd1-bc32-1a0f62f5726a"));
+
+            migrationBuilder.DeleteData(
+                schema: "business",
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: new Guid("d8f762ca-b7a5-486d-a793-e142f856d103"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_CoverPhotoId",
+                schema: "business",
+                table: "Companies",
+                column: "CoverPhotoId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Companies_Images_CoverPhotoId",
+                schema: "business",
+                table: "Companies",
+                column: "CoverPhotoId",
+                principalSchema: "business",
+                principalTable: "Images",
+                principalColumn: "ImageId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Companies_Images_CoverPhotoId",
+                schema: "business",
+                table: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_CoverPhotoId",
+                schema: "business",
+                table: "Companies");
+
+            migrationBuilder.InsertData(
+                schema: "business",
+                table: "Categories",
+                columns: new[] { "CategoryId", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("d8f762ca-b7a5-486d-a793-e142f856d103"), "Hairdresser" },
+                    { new Guid("40b22590-3f08-4928-a86c-5c1d5d75d3bb"), "Barber" },
+                    { new Guid("87467e7d-4185-4bd1-bc32-1a0f62f5726a"), "Massage" },
+                    { new Guid("3ac00a98-2b01-46d5-99f2-546b9e302b18"), "Makeup" }
+                });
+        }
+    }
+}
--- src/Modules/BusinessProfile/Servibes.BusinessProfile.Api/Models/Image.cs
+++ src/Modules/BusinessProfile/Servibes.BusinessProfile.Api/Models/Image.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace Servibes.BusinessProfile.Api.Models
 {
     public class Image
     {
         public Guid ImageId { get; set; }
+
         public string FileType { get; set; }
+
         [DataType(DataType.Upload)]
         public byte[] Data { get; set; }
     }
